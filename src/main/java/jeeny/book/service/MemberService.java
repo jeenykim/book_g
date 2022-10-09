f8ba37d02de7f10fea79dcd6f106eab089476ade
jeenykim <jeenykimsweb@gmail.com>
2022-10-09 19:20:09 +0900

    setting end

---
+++ src/main/java/jeeny/book/service/MemberService.java
@@ -0,0 +1,28 @@
+package jeeny.book.service;
+
+import jeeny.book.entity.Member;
+import jeeny.book.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService{
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
